e2d9d12c6200f32b88c8619bf40b6f0fa655f1f2
LucianoAlbanes <lucianoalbanes@gmail.com>
2021-05-11 20:50:44 -0300

    E6

---
+++ TP5/P2_E6.py
@@ -0,0 +1,43 @@
+'''
+Los nuevos códigos postales argentinos tienen la forma cddddccc, donde c indica un carácter
+(A - Z) y d indica un dígito 0, . . . , 9. Por ejemplo, C1024CWN es el código postal que
+representa a la calle XXXX a la altura 1024 en la Ciudad de Mendoza. Diseñar e implementar
+una función de hash apropiada para los códigos postales argentinos.
+'''
+
+# Define global constant
+A = ((5**.5 - 1)/2)  # Golden ratio φ
+
+# Define functions
+
+def h(cpa):
+    '''
+    Explanation:
+        Generates a hash from a given CPA (Código Postal Argentino).
+    Params:
+        cpa: An string of 8 characters, with numbers and letters (not case sensitive), A9999AAA.
+    '''
+    # Define hash value
+    hashSum = 0
+
+    # Check length condition
+    if len(cpa) != 8:
+        hashSum = None
+    else:
+        # Uppercase the CPA
+        cpa = cpa.upper()
+
+        # Do a summatory of the unicode code of each character
+        summatory = 0
+        for i in range(0, 8):
+            summatory += ord(cpa[i])
+
+        # Calc the hash of the summatory using the multiplication method (φ)
+        hashSum = int(m*(summatory*A % 1))
+
+    # Return the result value
+    return hashSum
+
+
+m = 100
+print(h('A9999AAA'))
