c0e8ccb43d243d8507742d7cadb96b6e477da8a1
LucianoAlbanes <lucianoalbanes@gmail.com>
2021-05-11 21:23:40 -0300

    Fix comment

--- TP5/P2_E6.py
+++ TP5/P2_E6.py
@@ -1,9 +1,3 @@
-'''
-Los nuevos códigos postales argentinos tienen la forma cddddccc, donde c indica un carácter
-(A - Z) y d indica un dígito 0, . . . , 9. Por ejemplo, C1024CWN es el código postal que
-representa a la calle XXXX a la altura 1024 en la Ciudad de Mendoza. Diseñar e implementar
-una función de hash apropiada para los códigos postales argentinos.
-'''
 
 # Define global constant
 A = ((5**.5 - 1)/2)  # Golden ratio φ
