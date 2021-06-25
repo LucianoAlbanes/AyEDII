876569933f621a68f2e6ea8466ecba115285b1d2
LucianoAlbanes <lucianoalbanes@gmail.com>
2021-06-25 20:47:12 -0300

    Ex2 Finished

--- TP8/lib/sortArray.py
+++ TP8/lib/sortArray.py
@@ -1,12 +1,25 @@
 from .algo1 import *
 
 # Insertion sort
-def insertionSort(array):
-    for i in range(1, len(array)):
-        key = array[i]
-        j = i-1
-        while j >= 0 and key < array[j]:
-            array[j + 1] = array[j]
+
+def insertionSortR(array):
+    '''
+    Returns a new array sorted in reverse order.
+    '''
+    # Define a new array and copy elements
+    outputArray = Array(len(array), 0)
+    for i in range(len(array)):
+        outputArray[i] = array[i]
+
+    # Sort in reverse order
+    for i in range(1, len(outputArray)):
+        temp = outputArray[i]
+        j = i - 1
+
+        while j >= 0 and temp > outputArray[j]:
+            outputArray[j+1] = outputArray[j]
+            outputArray[j] = temp
             j -= 1
-        array[j + 1] = key
-    return array
+
+    # Return sorted array
+    return outputArray
